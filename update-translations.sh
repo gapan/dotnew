dcd4514037d07dfba80d7a16473f381a614083cb
gapan <gapan@75335105-192a-4411-a05e-5a7f64d04acd>
2010-08-21 20:06:46 +0000

    Updated update-translations.sh script for including translations from dotnew-gtk

--- update-translations.sh
+++ update-translations.sh
@@ -3,9 +3,22 @@
 intltool-extract --type="gettext/ini" dotnew.desktop.in
 intltool-extract --type="gettext/ini" dotnew-kde.desktop.in
 sed -i '/char \*s = N_("Dotnew");/d' *.in.h
-xgettext --from-code=utf-8 -L shell -o po/dotnew.pot dotnew
+xgettext --from-code=utf-8 -L shell -o po/dotnew.pot src/dotnew
 xgettext --from-code=utf-8 -j -L C -kN_ -o po/dotnew.pot dotnew.desktop.in.h
 xgettext --from-code=utf-8 -j -L C -kN_ -o po/dotnew.pot dotnew-kde.desktop.in.h
+
+xgettext --from-code=utf-8 \
+	-j \
+	-x po/EXCLUDE \
+	-L Glade \
+	-o po/dotnew.pot \
+	src/dotnew-gtk.glade
+
+xgettext --from-code=utf-8 \
+	-j \
+	-L Python \
+	-o po/dotnew.pot \
+	src/dotnew-gtk
 
 rm dotnew.desktop.in.h dotnew-kde.desktop.in.h
 
